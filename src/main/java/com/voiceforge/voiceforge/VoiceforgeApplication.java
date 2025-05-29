ad2310fcef752fa912948347fff2f5bfc2c99897
Zainab Lawal <zainablaw1012@gmail.com>
2025-05-29 16:15:58 +0300

    Init: Project setup

---
+++ src/main/java/com/voiceforge/voiceforge/VoiceforgeApplication.java
@@ -0,0 +1,13 @@
+package com.voiceforge.voiceforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voiceforge/voiceforge/VoiceforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voiceforge.voiceforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
